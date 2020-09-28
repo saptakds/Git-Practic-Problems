17da5237a11b871b3855b9cf589f37e31355cbea
saptakds <saptakds@gmail.com>
2020-09-28 20:56:13 +0530

    case UC2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 randomVar=$((1+$RANDOM%2))
+attend=0
 if [ $randomVar -eq 1 ]
 then
+	attend=1
 	echo "Employee: Present"
 else
+	attend=0
 	echo "Employee: Absent"
 fi
+wageRate=20
+fullDayHour=8
+dailyWage=$(($wageRate*$fullDayHour*$attend))
+echo "Daily Employee Wage: $dailyWage"
