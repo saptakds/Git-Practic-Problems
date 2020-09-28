ddb5c4e9c36a63bc5d7b5ed903e5829a7dc52240
saptakds <saptakds@gmail.com>
2020-09-28 21:56:38 +0530

    case UC4

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 randomVar=$((1+$RANDOM%2))
-attend=0
-if [ $randomVar -eq 1 ]
-then
-	attend=1
-	echo "Employee: Present"
-else
-	attend=0
-	echo "Employee: Absent"
-fi
+dailyWage=0
+partTimeWage=0
 wageRate=20
 fullDayHour=8
-dailyWage=$(($wageRate*$fullDayHour*$attend))
-echo "Daily Employee Wage: $dailyWage"
 partTimeHour=8
-partTimeWage=$(($partTimeHour*$wageRate*$attend))
+case $randomVar in
+	1)
+		echo "Employee: Present"
+		dailyWage=$(($wageRate*$fullDayHour))
+		partTimeWage=$(($partTimeHour*$wageRate))
+	;;
+	2)
+		echo "Employee: Absent"
+	;;
+esac
+echo "Daily Employee Wage: $dailyWage"
 echo "Parttime wage: $partTimeWage"
