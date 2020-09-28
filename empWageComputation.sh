7d0b32b1a536033ecf02331b584914fcee31aff5
saptakds <saptakds@gmail.com>
2020-09-28 20:37:57 +0530

    case UC1

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+randomVar=$((1+$RANDOM%2))
+if [ $randomVar -eq 1 ]
+then
+	echo "Employee: Present"
+else
+	echo "Employee: Absent"
+fi
